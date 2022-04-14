c76b44270fe054d25ab15a1817504f491ac7b64b
afflrmative <smaill.pro3328@gmail.com>
2022-04-14 18:36:53 +0300

    swagger

--- src/modules/todo/controllers/todo.controller.ts
+++ src/modules/todo/controllers/todo.controller.ts
@@ -1,19 +1,34 @@
 import { Body, Controller, Delete, Get, HttpException, HttpStatus, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Todo } from '../entities/todo.entity';
 import { TodoService } from '../services/todo.service';
 import { CreateDto, updateDto } from './dto';
+import { NotFoundResponse } from './type';
 
-
+@ApiTags('todo')
 @Controller('rest/todo')
 export class TodoController {
     constructor(private readonly todoService: TodoService){}
 
   @Get()
+  @ApiResponse({ 
+    status: 200, 
+    description: 'get all todo',
+    type: [Todo]})
   getAllAction(): Promise<Todo[]> {
     return this.todoService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({ 
+    status: 200, 
+    description: 'get todo by id',
+    type: Todo})
+  @ApiResponse({ 
+    status: 404, 
+    description: 'NotFound',
+    type: NotFoundResponse
+  })
   async getOneAction(@Param('id') id: string): Promise<Todo> {
     const todo = await this.todoService.findOne(id);
     if (todo === undefined){
@@ -25,6 +40,16 @@ export class TodoController {
   }
 
   @Post()
+  @ApiResponse({ 
+    status: 200, 
+    description: 'create todo',
+    type: Todo})
+  @ApiResponse({ 
+    status: 404, 
+    description: 'NotFound',
+    type: NotFoundResponse
+  })
+  @ApiBody({ type: CreateDto })
   createAction(@Body() createDto: CreateDto): Promise<Todo> {
     const todo = new Todo();
     todo.title = createDto.title;
@@ -35,6 +60,16 @@ export class TodoController {
   }
 //Create
   @Put(':id')
+  @ApiResponse({ 
+    status: 200, 
+    description: 'update todo',
+    type: Todo})
+  @ApiResponse({ 
+    status: 404, 
+    description: 'NotFound',
+    type: NotFoundResponse
+  })
+  @ApiBody({ type: updateDto })
   async updateAction(
     @Param('id') id: string, 
     @Body() {title, isCompleted = false}: updateDto ): 
@@ -49,6 +84,15 @@ export class TodoController {
   }
 
   @Delete(':id')
+  @ApiResponse({ 
+    status: 200, 
+    description: 'delete todo'
+  })
+  @ApiResponse({ 
+    status: 404, 
+    description: 'NotFound',
+    type: NotFoundResponse
+  })
   async deleteAction(@Param('id') id: string): Promise<{success: boolean}>{
        await this.todoService.remove(id);
        return{
